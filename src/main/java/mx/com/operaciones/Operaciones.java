2681f263de89799a346d198c7bf43af93b9125be
Jorge Aguiar Junior <08220149@aluno.osorio.ifrs.edu.br>
2023-07-06 17:18:35 -0300

    Adicionado um método para testes no jenkins

--- src/main/java/mx/com/operaciones/Operaciones.java
+++ src/main/java/mx/com/operaciones/Operaciones.java
@@ -5,4 +5,6 @@ public interface Operaciones {
     double resta(double numero1, double numero2);
     double multiplicacion(double numero1, double numero2);
     double division(double numero1, double numero2);
+    // Método adicionado para teste do jenkins
+    double subtracion(double num1, double num2);
 }
